1c3f8cc57fd8b5c473cad9ea90345030af9bc30a
Patrick Campbell <pat@ptcampbell.com>
2021-08-10 21:54:31 -0700

    style: add fonts, color, typescript, logo

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background: #727272;
+    color:#fff;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Work Sans', Arial, Helvetica, sans-serif;
+    font-size: 24px;
+	  line-height: 150%;
+  }
+`
+
+const theme = {
+	colors: {
+		primary: '#0070f3',
+	},
+}
+
+export default function App({ Component, pageProps }) {
+	return (
+		<>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	)
+}
